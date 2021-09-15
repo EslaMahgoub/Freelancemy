c084d65ab2687294df9cf96a9da1a16ff207cc64
exit <exit>
2021-09-15 19:10:21 +0200

    New Proposal Component Styling

--- src/app/proposal/proposal-new.component.ts
+++ src/app/proposal/proposal-new.component.ts
@@ -4,6 +4,7 @@ import { Proposal } from "./proposal";
 @Component({
   selector: 'proposal-new',
   templateUrl: 'proposal-new.component.html',
+  styleUrls: ['proposal-new.component.css']
 })
 
 export class ProposalNewComponent {
