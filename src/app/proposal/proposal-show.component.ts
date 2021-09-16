c49bd0bc5f06cc488804b877da33121f5dcc1eb6
exit <exit>
2021-09-16 19:14:00 +0200

    Get Proposal show components data through api

--- src/app/proposal/proposal-show.component.ts
+++ src/app/proposal/proposal-show.component.ts
@@ -1,25 +1,31 @@
-import { Component, OnInit } from "@angular/core";
+import { Component, Input } from "@angular/core";
 import { ActivatedRoute, Params } from "@angular/router";
+import {HttpClient, HttpResponse, HttpHeaders , HttpRequest} from "@angular/common/http";
+import { Observable } from 'rxjs';
 import { Proposal } from "./proposal";
+import { ProposalService } from "./proposal.service";
+
+
 @Component({
   selector: "proposal-show",
   templateUrl: "proposal-show.component.html",
   styleUrls: ["proposal-show.component.css"],
+  providers: [ProposalService]
 })
 
-export class ProposalShowComponent implements OnInit {
-  id: number;
-  routeId: any;
-
+export class ProposalShowComponent{
   constructor(
-    private route:ActivatedRoute
-
+    private route:ActivatedRoute,
+    private proposalService: ProposalService,
+    private httpClient: HttpClient,
   ) {}
 
+  @Input() proposal: Proposal;
+
   ngOnInit(): void {
-    this.routeId = this.route.params.subscribe(
-      params => {
-        this.id = +params['id']; // '+' converting for a number
-    })
+    this.route.params.subscribe((params: Params) => {
+          this.proposalService.getProposal(+params['id']).subscribe(
+            data => this.proposal = data);
+          }
+        )};
   }
-}; 
